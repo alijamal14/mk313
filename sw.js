e58e9e5bf4f4d2b494e6f2b1f0591e7eab4ba906
Ali Jamal <alijamal14@live.com>
2025-10-27 17:06:27 +0500

    feat: expand offerings, assets, and PWA support for mk313

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'mk313-cache-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/images/logo/logo.jpg',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const { request } = event;
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(cachedResponse => {
+      if (cachedResponse) {
+        fetch(request).then(response => {
+          caches.open(CACHE_NAME).then(cache => cache.put(request, response.clone()));
+        }).catch(() => {});
+        return cachedResponse;
+      }
+
+      return fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match('/index.html'));
+    })
+  );
+});
